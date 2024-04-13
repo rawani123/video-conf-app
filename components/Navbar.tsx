d166f8d15530203f546460c0fa972e3c19d65ab1
Aniruddha Rawool <aniruddha.212074101@vcet.edu.in>
2024-04-13 10:39:41 +0530

    add date and time

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       </Link>
       <div className='flex-between gap-5 '>
         <SignedIn>
-          <UserButton/>
+          <UserButton afterSignOutUrl='/sign-in'/>
         </SignedIn>
         <MobileNav/>
       </div>
